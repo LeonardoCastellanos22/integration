36130ed058527c01ed5fc45f5d3c449bc2387f72
LeonardoCastellanos22 <edgarleonardo.castellanos@gmail.com>
2023-07-11 16:06:07 -0500

    Adding Uvicorn

--- main.py
+++ main.py
@@ -314,5 +314,6 @@ def create_request(url, headers, request_type, data=None, cookies = None):
     
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))

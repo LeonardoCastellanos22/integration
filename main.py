b38652617a66b027a64add0e4ee1f640d06a3aaf
LeonardoCastellanos22 <105248861+LeonardoCastellanos22@users.noreply.github.com>
2023-07-11 16:24:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -316,5 +316,4 @@ def create_request(url, headers, request_type, data=None, cookies = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0",
-                port=int(os.environ.get("PORT", 7126)))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
